eb012b8f5bb55d8c24c887b7bd2b0a9915fc54be
Alena Lisicyna <alelisicyna@gmail.com>
2024-12-21 16:30:50 +0300

    Delete legacy-frontend

--- legacy-frontend/static/js/kanvertar.js
+++
@@ -1,8 +0,0 @@
-function copy_to_clipboard(id) {
-    var text = document.getElementById(id).innerHTML;
-    navigator.clipboard.writeText(text);
-    document.getElementById("copy").innerHTML="СКАПІЯВАНА!";
-    setTimeout(function() {
-        document.getElementById("copy").innerHTML="СКАПІЯВАЦЬ";
-    }, 2000);
-};
